3e10758db6b71c0a67dafa4b74171335153aadcd
Randy <rls614sm@charter.net>
2017-03-18 22:49:19 -0500

    Update UnorderedList.scala

--- UnorderedList.scala
+++ UnorderedList.scala
@@ -1,1 +1,8 @@
-
+val words = List("Air", "Liquid", "Solid", "Alphabet",  "Steam", "Elements", "Alphanumeric", "Scala" )
+    words.foreach(x => println(x)) 
+      
+val alphaSort = words.sorted
+    alphaSort.foreach(x => println(x))
+      
+val sizeSort = words.sortBy(_.length)
+    sizeSort.foreach(x => println(x))
